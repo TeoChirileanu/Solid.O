e9893253a9c4c24d27c18bb7fe12876031b60062
Teodor <asdasd@asdas.com>
2019-01-20 18:42:46 +0100

    added consoleapp

---
+++ Solid.O.ConsoleApplication/ConsoleApplication.cs
@@ -0,0 +1,13 @@
+﻿using Solid.O.Libraries;
+
+namespace Solid.O.ConsoleApplication {
+    internal static class ConsoleApplication {
+        private static readonly IGetter Getter = new ConsoleGetter();
+        private static readonly IDeliverer Deliverer = new ConsoleDeliverer();
+
+        private static void Main() {
+            var message = Getter.Get();
+            Deliverer.Deliver(message);
+        }
+    }
+}
