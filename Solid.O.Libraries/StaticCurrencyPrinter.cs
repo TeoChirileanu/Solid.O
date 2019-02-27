6da4028184197dc9cd71ffb03048a6745789c712
Teodor <asdasd@asdas.com>
2019-02-27 21:21:03 +0100

    renamed printer to static printer to differentiate it from the dynamic printer

--- Solid.O.Libraries/StaticCurrencyPrinter.cs
+++ Solid.O.Libraries/StaticCurrencyPrinter.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
 namespace Solid.O.Libraries {
-    public static class CurrencyPrinter {
+    public static class StaticCurrencyPrinter {
         public static void PrintCurrency<T>() where T : ICurrency, new() {
             var currency = new T();
             var message = $"Currency {currency.Abbreviation}\n" +
                           $"Symbol: {currency.Symbol}\n" +
-                          $"Exchange rate: 1 {currency.Symbol} = {currency.ExchangeRate} EUR";
+                          $"Exchange rate: 1 {currency.Symbol} = {currency.ExchangeRate} EUR\n";
             Console.WriteLine(message);
         }
     }
