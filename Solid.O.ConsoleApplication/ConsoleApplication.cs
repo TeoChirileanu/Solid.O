75f68a03cbf4c8a8426f4e57dfe329e6684cb31c
Teodor <asdasd@asdas.com>
2019-02-27 21:21:13 +0100

    updated usages

--- Solid.O.ConsoleApplication/ConsoleApplication.cs
+++ Solid.O.ConsoleApplication/ConsoleApplication.cs
@@ -2,23 +2,35 @@
 using System.Text;
 using Solid.O.Libraries;
 using Solid.O.Libraries.Currencies;
+using Solid.O.Libraries.DynamicCurrencyPrinters;
 
 namespace Solid.O.ConsoleApplication {
     public static class ConsoleApplication {
         public static void Main() {
             Console.OutputEncoding = Encoding.UTF8;
-            UseStaticPolymorphism();
             UseDynamicPolymorphism();
+            UseStaticPolymorphism();
         }
 
         private static void UseStaticPolymorphism() {
-            CurrencyPrinter.PrintCurrency<Euro>();
-            CurrencyPrinter.PrintCurrency<PoundSterling>();
-            CurrencyPrinter.PrintCurrency<UnitedStatesDollar>();
+            Console.WriteLine("Printing using static polymorphism:\n");
+
+            StaticCurrencyPrinter.PrintCurrency<Euro>();
+            StaticCurrencyPrinter.PrintCurrency<PoundSterling>();
+            StaticCurrencyPrinter.PrintCurrency<UnitedStatesDollar>();
         }
 
         private static void UseDynamicPolymorphism() {
-            throw new NotImplementedException();
+            Console.WriteLine("Printing using Dynamic Polymorphism\n");
+
+            DynamicCurrencyPrinter currencyPrinter = new EuroPrinter();
+            currencyPrinter.PrintCurrency();
+
+            currencyPrinter = new PoundSterlingPrinter();
+            currencyPrinter.PrintCurrency();
+
+            currencyPrinter = new UnitedStatesDollarPrinter();
+            currencyPrinter.PrintCurrency();
         }
     }
 }
