e2d95b284336cfe45c4324efb2559863c39f8afa
Teodor <asdasd@asdas.com>
2019-02-27 20:49:10 +0100

    implemented static polymorphism

---
+++ Solid.O.Libraries/CurrencyPrinter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Solid.O.Libraries {
+    public static class CurrencyPrinter {
+        public static void PrintCurrency<T>() where T : ICurrency, new() {
+            var currency = new T();
+            var message = $"Currency {currency.Abbreviation}:\n" +
+                          $"Symbol: {currency.Symbol}\n" +
+                          $"Exchange rate: 1 {currency.Symbol} = {currency.ExchangeRate} EUR";
+            Console.WriteLine(message);
+        }
+    }
+}
